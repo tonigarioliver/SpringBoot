beb8503fbff6f63599e9c1c62069094de964d8a6
tonigarioliver <tonietmoscari@gmail.com>
2023-02-13 18:02:12 +0000

    start swagger

--- ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
+++ ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/ObRestDatajpaApplication.java
@@ -12,9 +12,11 @@ import java.time.LocalDate;
 public class ObRestDatajpaApplication {
 
 	public static void main(String[] args) {
-
-		ApplicationContext context=SpringApplication.run(ObRestDatajpaApplication.class, args);
+		ApplicationContext context = SpringApplication.run(ObRestDatajpaApplication.class, args);
 		BookRepository repository = context.getBean(BookRepository.class);
+
+		// CRUD
+		// crear libro
 		Book book1 = new Book(null, "Homo Deus", "Yuval Noah", 450, 29.99, LocalDate.of(2018, 12, 1), true);
 		Book book2 = new Book(null, "Homo Sapiens", "Yuval Noah", 450, 19.99, LocalDate.of(2013, 12, 1), true);
 		// almacenar un libro
