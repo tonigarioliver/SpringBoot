c684beaadc8966590f2a9f4a5389e800d48cae33
tonigarioliver <tonietmoscari@gmail.com>
2023-02-14 17:54:59 +0000

    add testing unit

---
+++ ob-rest-datajpa/src/test/java/com/example/obrestdatajpa/controller/BookControllerTest.java
@@ -0,0 +1,49 @@
+package com.example.obrestdatajpa.controller;
+
+import com.example.obrestdatajpa.entities.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookControllerTest {
+
+    private TestRestTemplate testRestTemplate;
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+    @LocalServerPort
+    private int port;
+    @BeforeEach
+    void setUp() {
+        restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:"+port);
+        testRestTemplate =  new TestRestTemplate(restTemplateBuilder);
+    }
+
+    @Test
+    void findAll() {
+        ResponseEntity<Book[]> response = testRestTemplate.getForEntity("/api/books", Book[].class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        List<Book> books = Arrays.asList(response.getBody());
+        System.out.println(books.size());
+    }
+
+    @Test
+    void findOneById() {
+    }
+
+    @Test
+    void create() {
+    }
+}
