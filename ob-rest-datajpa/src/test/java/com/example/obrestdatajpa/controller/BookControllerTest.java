22e2fc917c9a8ba787d6441ad50aa4041c20df67
tonigarioliver <tonietmoscari@gmail.com>
2023-02-15 13:10:33 +0000

    finishtin test junit

--- ob-rest-datajpa/src/test/java/com/example/obrestdatajpa/controller/BookControllerTest.java
+++ ob-rest-datajpa/src/test/java/com/example/obrestdatajpa/controller/BookControllerTest.java
@@ -2,14 +2,14 @@ package com.example.obrestdatajpa.controller;
 
 import com.example.obrestdatajpa.entities.Book;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -41,9 +41,36 @@ class BookControllerTest {
 
     @Test
     void findOneById() {
+        ResponseEntity<Book>response =  testRestTemplate.getForEntity("/api/books/1",Book.class);
+        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
     }
-
+    @DisplayName("Comprobar create controladores Spring REST")
     @Test
     void create() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        String json = """
+                {
+                    "title": "Libro creado desde Spring Test",
+                    "author": "Yuval Noah",
+                    "pages": 650,
+                    "price": 19.99,
+                    "releaseDate": "2019-12-01",
+                    "online": false
+                }
+                """;
+
+        HttpEntity<String> request = new HttpEntity<>(json,headers);
+
+        ResponseEntity<Book> response = testRestTemplate.exchange("/api/books", HttpMethod.POST, request, Book.class);
+
+        Book result = response.getBody();
+
+        assertEquals(1L, result.getId());
+        assertEquals("Libro creado desde Spring Test", result.getTitle());
+
     }
 }
